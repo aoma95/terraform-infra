2bc94dbbb51c8b1b8f8b405b94571789455da111
Dan <dan.esposito@ynov.com>
2023-01-06 16:48:51 +0100

    ajout prenom-index

--- exo1.tf
+++ exo1.tf
@@ -21,14 +21,14 @@ resource "scaleway_vpc_private_network" "pn_priv" {
 resource "scaleway_instance_server" "mes-instances-DEV1-S" {
   count = 3
   type = "DEV1-S"
-  name = "instance-DEV1-S-${count.index}"
+  name = "dan-${count.index}"
   image = "ubuntu_focal"
 }
 # Instance DEV-XL
 resource "scaleway_instance_server" "mes-instances-DEV1-XL" {
   image = "ubuntu_focal"
   type  = "DEV1-XL"
-  name = "instance-DEV1-XL-${count.index}"
+  name = "dan-${count.index}"
 }
 resource "scaleway_instance_volume" "server_volume" {
     type       = "l_ssd"
