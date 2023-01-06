06f8ef8c27b99e394e8315814ecc1191b80e6196
Dan <dan.esposito@ynov.com>
2023-01-06 16:08:22 +0100

    ajout du provider scaleway avec réseau privé  pn_priv

--- exo1.tf
+++ exo1.tf
@@ -1,1 +1,19 @@
-# Ajout d'un provider Scaleway
+terraform {
+  required_providers {
+    scaleway = {
+      source = "scaleway/scaleway"
+      version = "2.9.0"
+    }
+  }
+}
+
+provider "scaleway" {
+  # Configuration options
+  zone   = "fr-par-1"
+  region = "fr-par"
+}
+
+resource "scaleway_vpc_private_network" "pn_priv" {
+    name = "pn_priv"
+
+}
