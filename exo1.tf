967ec275a1bdb71e9d00741fc307f55d41610441
Dan <dan.esposito@ynov.com>
2023-01-06 16:44:12 +0100

    Ajout d'un disque additionnel sur chaque instance pour le stockage des données (30G)

--- exo1.tf
+++ exo1.tf
@@ -30,3 +30,18 @@ resource "scaleway_instance_server" "mes-instances-DEV1-XL" {
   type  = "DEV1-XL"
   name = "instance-DEV1-XL-${count.index}"
 }
+resource "scaleway_instance_volume" "server_volume" {
+    type       = "l_ssd"
+    name       = "petit-volume"
+    size_in_gb = 30
+}
+# Attachement du volume à l'instance DEV1-S
+resource "scaleway_instance_volume_attachment" "mes-instances-DEV1-S" {
+  instance_id = scaleway_instance_server.mes-instances-DEV1-S.id
+  volume_id   = scaleway_volume.server_volume.id
+}
+# Attachement du volume à l'instance DEV1-XL
+resource "scaleway_instance_volume_attachment" "mes-instances-DEV1-XL" {
+  instance_id = scaleway_instance_server.mes-instances-DEV1-XL.id
+  volume_id   = scaleway_volume.server_volume.id
+}
