51aa4684cb7e9e2b3034a4bbba9dc600fe58a16c
Dan <dan.esposito@ynov.com>
2023-01-19 15:21:26 +0100

    refactory

--- modules/instances/variables.tf
+++ modules/instances/variables.tf
@@ -18,9 +18,15 @@ variable "servers" {
     image = string
     type = string
   }))
-  default = [
-    for i in range(3) : {name = "Dan_DEV1_S${i+1}", image = "ubuntu_focal", type = "DEV1-S"}
-  ] + [
-    {name = "Dan_DEV1_XL1", image = "ubuntu_focal", type = "DEV1-XL"}
+    default = [
+    {name = "Dan_DEV1_S_1", image = "ubuntu_focal", type = "DEV1-S"},
+    {name = "Dan_DEV1_S_2", image = "ubuntu_focal", type = "DEV1-S"},
+    {name = "Dan_DEV1_S_3", image = "ubuntu_focal", type = "DEV1-S"},
+    {name = "Dan_DEV1_XL_1", image = "ubuntu_focal", type = "DEV1-XL"}
   ]
+  # default = [
+  #   for i in range(3) : {name = "Dan_DEV1_S${i+1}", image = "ubuntu_focal", type = "DEV1-S"}
+  # ] + [
+  #   {name = "Dan_DEV1_XL1", image = "ubuntu_focal", type = "DEV1-XL"}
+  # ]
 }
